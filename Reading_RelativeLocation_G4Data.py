33332c3160001cdf82d12f4e5f574902fa014da5
Roksana Sadeghi <roksana.s.sadeghi@gmail.com>
2022-09-01 16:09:12 -0400

    return the sorted combined sessions as a text file in the same input directory

--- Reading_RelativeLocation_G4Data.py
+++ Reading_RelativeLocation_G4Data.py
@@ -7,7 +7,7 @@ Created on Wed Aug 31 13:42:31 2022
 
 import os
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #reading the directories and files
 def FileDir( directory, nametag ):
     sessiondir = []
@@ -62,20 +62,27 @@ def FitLineG4(sensor):
     x1 = sensor[-1,1]
     y0 = x0 * a + b
     y1 = x1 * a + b
-    return np.degrees(np.arctan2(x1-x0,y1-y0))
+    return np.round(np.degrees(np.arctan2(y1-y0,x1-x0)),2)
 
 #fit a line to sensor1 and sensor2 data and find the angle and update the param line
 def AddAngleToStimParametersLine(param, g4Data):
     sensor1,sensor2 = SeparateSensors(g4Data)
-    if (np.std(sensor1[:,1]) < 1.0) & (np.std(sensor1[:,0]) < 1.0):
+    angle1 = "nan"
+    angle2 = "nan"
+    if len(sensor1)<8 or len(sensor2)<8:
+        angle1 = "nan"
+        angle2 = "nan"
+    elif ((np.std(sensor1[:,1]) > 1.0) or (np.std(sensor1[:,0]) > 0.1)):
         angle1 = FitLineG4(sensor1)
+    elif ((np.std(sensor2[:,1]) > 1.0) or (np.std(sensor2[:,0]) > 1.0)):
         angle2 = FitLineG4(sensor2)
     else:
-        angle1 = []
-        angle2 = []
-    parameters = np.hstack((np.array(param.replace("\n","").split(" ; ")),angle1,angle2))
+        angle1 = "nan"
+        angle2 = "nan"
+    parameters = " ; ".join(np.hstack((np.array(param.replace("\n","").split(" ; ")),angle1,angle2)))
     return parameters
 
+#combine all trials 
 def CombineAllTrials(mainDir):
     trials = []
     for item in FileDir(mainDir, "Session"):
@@ -85,10 +92,70 @@ def CombineAllTrials(mainDir):
             trials.append(AddAngleToStimParametersLine(param, g4Data))
     return trials
 
-    
-CombineAllTrials(os.getcwd() + "\RelativeMappingData")
+#separate the unique electrode groups and save them in an array for each trial
+def SeparateElecGroups(allTrials):
+    electrode = []
+    for trial in allTrials:
+        trial_electrode = np.array(trial.split(" ; ")[2].split(" , "))
+        trial_electrode[1] = np.char.strip( trial_electrode[1].replace(trial_electrode[0],"") , chars = '-')
+        electrode.append(trial_electrode)
+    return np.array(electrode)        
 
+def SortTrials(electrode,allTrials):
+    pairs = set()
+    index = []
+    for i in range(0,len(electrode)):
+        if ((electrode[i,0],electrode[i,1]) in pairs) or ((electrode[i,1],electrode[i,0]) in pairs):continue
+        else:
+            pairs.add((electrode[i,0],electrode[i,1]))
+            ind1 = electrode==[electrode[i,0],electrode[i,1]]
+            ind2 = electrode==[electrode[i,1],electrode[i,0]]
+            index.append( np.hstack((np.where(ind1[:,0]&ind1[:,1]),np.where(ind2[:,0]&ind2[:,1]))) )
+    
+    allTrials = np.array(allTrials)
+    allTrials_sorted = []
+    electrode_sorted = []
+    for i in range(0,len(index)):
+        allTrials_sorted.append( allTrials[index[i]] )
+        electrode_sorted.append( electrode[index[i],:] )     
         
+    return allTrials_sorted
+
+def SaveSortedTrials(allTrials_sorted, mainDir):
+    textFile = mainDir + "\\RelativeLocation_AllTrials.txt"
+    with open(textFile,'w') as f:
+        for groups in allTrials_sorted:
+            f.writelines('\n'.join(groups[0,:]) + '\n')
+    
+
+mainDir = os.getcwd() + "\RelativeMappingData"
+allTrials = CombineAllTrials(mainDir)
+electrode = SeparateElecGroups(allTrials)
+allTrials_sorted = SortTrials(electrode,allTrials)
+SaveSortedTrials(allTrials_sorted, mainDir)
+
+mainDir = os.getcwd() + "\RelativeMappingData"        
+for item in FileDir(mainDir, "Session"):
+    g4Files = FileDir(mainDir + "\\" + item, "G4")
+    for g4 in g4Files:
+        param,g4Data = ReadG4(mainDir + "\\" + item + "\\" + g4)
+        sensor1,sensor2 = SeparateSensors(g4Data)
+        if len(sensor1)<8 or len(sensor2)<8:
+            angle1 = []
+            angle2 = []
+        elif ((np.std(sensor1[:,1]) < 1.0) and (np.std(sensor1[:,0]) < 1.0)):
+            angle1 = FitLineG4(sensor1)
+            angle2 = FitLineG4(sensor2)
+        else:
+            angle1 = []
+            angle2 = []
+        parameters = np.hstack((np.array(param.replace("\n","").split(" ; ")),angle1,angle2))
+        
+
+
+
+
+
 
 
 
